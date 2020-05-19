e0b88b4db7d012b4c5bf6c0e1ebc02b2a78797da
Robert <roman.ayre@gmail.com>
2020-05-19 18:30:03 -0500

    cambiando config

--- estacion/settings.py
+++ estacion/settings.py
@@ -16,6 +16,7 @@ from django.core.urlresolvers import reverse_lazy
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -129,13 +130,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-#RobertERROR#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Esta es una Variable para Produccion (Robert)
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+# Esta es una Variable para Produccion (Robert)
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles') 
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+#Error:#STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 STATIC_URL = '/static/'
 
 #RobertERROR#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),'/static/',)
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = [os.path.join(PROJECT_ROOT, 'static'), ]
 
 #RobertPRUEBA#STATICFILES_STORAGES = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
